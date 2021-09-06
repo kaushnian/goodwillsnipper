db0a09255e2fc7cdd0496c58784b5dbf415bb4b8
kaushnian <kefbox@gmail.com>
2021-09-06 19:19:04 -0400

    Popup ui

--- src/background.js
+++ src/background.js
@@ -7,7 +7,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {
     const message = `Hi ${
       sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+    }, my name is Bac. I am from Background.`;
 
     // Log message coming from the `request` parameter
     console.log(request.payload.message);
